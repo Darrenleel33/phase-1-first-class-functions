03f778a59e9496e8657056cfc0baa5c4e42460cc
Darren <DarrenLeel33@gmail.com>
2021-05-19 10:43:38 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    const fn = () => console.log('returns a named function')
+    return fn
+}
+function returnsAnAnonymousFunction(){
+    return() => console.log('returns an anonymous function')
+}
